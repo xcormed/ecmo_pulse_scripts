00a2364385a92def88c47391f275d8c2786ff56b
Elizabeth Dietrich <elizabethdietrich@Elizabeths-MacBook-Pro.local>
2022-05-10 21:53:35 -0400

    add ecmo script

--- disease_scripts/COPD_ecmo.py
+++ disease_scripts/COPD_ecmo.py
@@ -36,7 +36,7 @@ def COPD_ecmo(patient, level_severity):
         SEDataRequest.create_substance_request("Sodium", "Clearance-RenalClearance", unit=VolumePerTimeMassUnit.mL_Per_min_kg),
         SEDataRequest.create_substance_request("Sodium", "MassInBody", unit=MassUnit.g),
         SEDataRequest.create_liquid_compartment_substance_request("Aorta", "CarbonDioxide", "PartialPressure", unit=PressureUnit.mmHg),
-        SEDataRequest.create_liquid_compartment_substance_request("Aorta", "Oxygen", "PartialPressure", unit=PressureUnit.mmHg),
+        SEDataRequest.create_liquid_compartment_substance_request("Aorta", "Oxygen", "PartialPressure", unit=PressureUnit.mmHg),\
         # ECMO Compartments
         SEDataRequest.create_liquid_compartment_request("ECMOBloodSamplingPort", "InFlow", unit=VolumePerTimeUnit.mL_Per_s),
         SEDataRequest.create_liquid_compartment_request("ECMOBloodSamplingPort", "OutFlow", unit=VolumePerTimeUnit.mL_Per_s),
@@ -45,8 +45,14 @@ def COPD_ecmo(patient, level_severity):
         SEDataRequest.create_liquid_compartment_substance_request("ECMOOxygenator", "Sodium", "Concentration", unit=MassPerVolumeUnit.g_Per_dL),
         SEDataRequest.create_liquid_compartment_substance_request("VenaCava", "Sodium", "Concentration", unit=MassPerVolumeUnit.g_Per_dL),
         SEDataRequest.create_liquid_compartment_substance_request("Aorta", "Sodium", "Concentration", unit=MassPerVolumeUnit.g_Per_dL),
-        SEDataRequest.create_liquid_compartment_substance_request("Aorta", "Oxygen", "Concentration", unit=MassPerVolumeUnit.g_Per_dL),
-        SEDataRequest.create_liquid_compartment_substance_request("Aorta", "CarbonDioxide", "Concentration", unit=MassPerVolumeUnit.g_Per_dL)
+        SEDataRequest.create_liquid_compartment_substance_request("VenaCava", "CarbonDioxide", "PartialPressure",
+                                                                  unit=PressureUnit.mmHg),
+        SEDataRequest.create_liquid_compartment_substance_request("VenaCava", "Oxygen", "PartialPressure",
+                                                                  unit=PressureUnit.mmHg),
+        SEDataRequest.create_liquid_compartment_substance_request("VenaCava", "Oxygen", "Concentration",
+                                                                  unit=MassPerVolumeUnit.g_Per_dL),
+        SEDataRequest.create_liquid_compartment_substance_request("VenaCava", "CarbonDioxide", "Concentration",
+                                                                  unit=MassPerVolumeUnit.g_Per_dL)
     ]
 
     # Produce a data manager object with the data request list, and tell it where to save
@@ -65,8 +71,8 @@ def COPD_ecmo(patient, level_severity):
     exacerbation.get_emphysema_severity().set_value(level_severity)
     pulse.process_action(exacerbation)
 
-    # Get the values of the data you requested at this time
-    results = pulse.pull_data()
+    # Advance 2 mins to get stabilize our patient to our COPD settings
+    pulse.advance_time_s(120)
 
     # Initialize ECMO
     cfg = SEECMOConfiguration()
@@ -74,75 +80,92 @@ def COPD_ecmo(patient, level_severity):
     # Attach ECMO from jugular to left leg
     settings.set_inflow_location(eECMO_CannulationLocation.InternalJugular)
     settings.set_outflow_location(eECMO_CannulationLocation.LeftFemoralVein)
-
     settings.get_oxygenator_volume().set_value(500, VolumeUnit.mL)
     settings.get_transfusion_flow().set_value(16.7, VolumePerTimeUnit.mL_Per_s)
+    pulse.process_action(cfg)
+    # Advance a little time to get ECMO connected and flowing
+    pulse.advance_time_s(10)
 
-    # Calculate the amount of O2 to add to the ECMO circuit and process the action
-    # O2 Added[g / min] = -0.0033 * (Blood Flow Rate[L / min]) ^ 2 + 0.1062 * (Blood Flow Rate[L / min]) - 0.0037
-    # O2 Added[g / min] = -0.0033 * (1)^2 + 0.1062 * 1  - 0.0037
-    # O2 Added[g / min] = -0.0033 + 0.1062 - 0.0037
-    # O2 Added[g / min] = .0992
-    # O2 Added[g / L] = (.0992 / 1) = .0992
-    # O2 Added[g / dL] = 0.00992
-    # add this value of O2 to the amount of O2 already in
-    print(results[list(results)[28]][-1])
-    settings.get_substance_concentration("Oxygen").get_concentration().set_value(results[list(results)[28]][-1]+0.00992
-                                                                                 , MassPerVolumeUnit.g_Per_dL)
+    # Clear our settings, as we don't need to specify our initial values each time
+    settings.clear()
+    results = pulse.pull_data()
+    data_mgr.to_console(results)
+
     # Calculate the amount of CO2 to remove from the ECMO circuit and process the action
     # CO2 Removed[g / min] = -0.0057 * (Blood Flow Rate[L / min]) ^ 2 + 0.1276 * (Blood Flow Rate[L / min]) + 0.0105
     # CO2 Removed[g / min] = -0.0057 * (1)^ 2 + 0.1276 * 1 + 0.0105
     # CO2 Removed[g / min] = -0.0057 + 0.1276 + 0.0105
     # CO2 Removed[g / min] = .1324
     # CO2 Removed[g / L] = .321 / 1 = .321
-    # CO2 Removed[g / dL] = .321 / 1 = .0321
+    # CO2 Removed[g / dL] = .0321
+    # Check that pCO2 must be >=15 mmHg
+    # pc02 before and after removing co2 to see how different it is each time
+    estimate = max((results[list(results)[26]][-1] - 15) * 0.0321, 0)
+    estimate_co2 = min(estimate, 0.0321)
     settings.get_substance_concentration("CarbonDioxide").get_concentration().set_value(
-        results[list(results)[29]][-1]-0.0321, MassPerVolumeUnit.g_Per_dL)
+        results[list(results)[29]][-1] - estimate_co2, MassPerVolumeUnit.g_Per_dL)
 
-    pulse.process_action(cfg)
-
-    results = pulse.pull_data()
+    # Get the values of the data you requested at this time
+    # Calculate the amount of O2 to add to the ECMO circuit and process the action
+    # O2 Added[g / min] = -0.0033 * (Blood Flow Rate[L / min]) ^ 2 + 0.1062 * (Blood Flow Rate[L / min]) - 0.0037
+    # O2 Added[g / min] = -0.0033 * (1.002)^2 + 0.1062 * 1.002  - 0.0037
+    # O2 Added[g / min] = -0.00331 + 0.1064 - 0.0037
+    # O2 Added[g / min] = .0994
+    # O2 Added[g / L] = (.0994 / 1.002) = .0992
+    # O2 Added[g / dL] = 0.00992
+    # add this value of O2 to the amount of O2 already in
     # Check that O2 saturation must not exceed 1 and pO2 must be <=300 mmHg
     # O2 added may be lower in order to fulfill this boundary condition
-    while results[list(results)[7]][-1] > 1.0 or results[list(results)[20]][-1] > 300:
-        current_oxygen = results[list(results)[28]][-1]
-        new_oxygen = current_oxygen * .999
-        settings.get_substance_concentration("Oxygen").get_concentration().set_value(new_oxygen,
-                                                                                     MassPerVolumeUnit.g_Per_dL)
-        pulse.process_action(cfg)
-        print("Recalculated oxygen")
-        results = pulse.pull_data()
+    if results[list(results)[7]][-1] < 1.0 or results[list(results)[27]][-1] <= 300:
+        settings.get_substance_concentration("Oxygen").get_concentration().set_value(
+            results[list(results)[28]][-1]+0.00992, MassPerVolumeUnit.g_Per_dL)
 
-    # Check that pCO2 must be >=15 mmHg
-    # CO2 removed may be lower in order to fulfill this boundary condition
-    while results[list(results)[19]][-1] < 15:
-        current_co2 = results[list(results)[29]][-1]
-        new_co2 = current_co2 * .999
-        settings.get_substance_concentration("CarbonDioxide").get_concentration().set_value(new_co2,
-                                                                                             MassPerVolumeUnit.g_Per_dL)
+    for i in range(900*50):
         pulse.process_action(cfg)
-        print("Recalculated carbon dioxide")
-        results = pulse.pull_data()
+        pulse.advance_time()
 
-    # Advance in time 900 seconds (15 minutes). This data will be saved to the csv.
-    pulse.advance_time_s(900)
+    # results = pulse.pull_data()
+    # # Check that O2 saturation must not exceed 1 and pO2 must be <=300 mmHg
+    # # O2 added may be lower in order to fulfill this boundary condition
+    # while results[list(results)[7]][-1] > 1.0 or results[list(results)[20]][-1] > 300:
+    #     current_oxygen = results[list(results)[28]][-1]
+    #     new_oxygen = current_oxygen*10 * .999
+    #     settings.get_substance_concentration("Oxygen").get_concentration().set_value(new_oxygen,
+    #                                                                                  MassPerVolumeUnit.g_Per_dL)
+    #     pulse.process_action(cfg)
+    #     print("Recalculated oxygen")
+    #     results = pulse.pull_data()
+    #
+    # # Check that pCO2 must be >=15 mmHg
+    # # CO2 removed may be lower in order to fulfill this boundary condition
+    # while results[list(results)[19]][-1] < 15:
+    #     current_co2 = results[list(results)[29]][-1]
+    #     new_co2 = current_co2*10 * .999
+    #     settings.get_substance_concentration("CarbonDioxide").get_concentration().set_value(new_co2,
+    #                                                                                          MassPerVolumeUnit.g_Per_dL)
+    #     pulse.process_action(cfg)
+    #     print("Recalculated carbon dioxide")
+    #     results = pulse.pull_data()
+    #
+    # # Advance in time 900 seconds (15 minutes). This data will be saved to the csv.
+    # pulse.advance_time_s(900)
     # And write it out to the console
     data_mgr.to_console(results)
 
-
-if __name__ == '__main__':
-    # # Simulate across all 10 patients, giving each severities of 0.3, 0.6, and 0.9
-    names = ["Cynthia", "Gus", "Joel", "Nathan", "Rick", "Hassan", "Soldier", "Jeff", "Carol", "Jane"]
-    severities = [0.3, 0.6, 0.9]
-    processes = []
-
-# Add a new thread for every patient at each severity, start each thread, and join them
-    for name in names:
-        for severity in severities:
-            processes.append(mp.Process(None, COPD_ecmo, args=(name, severity)))
-
-    for p in processes:
-        p.start()
-
-    for p in processes:
-        p.join()
+# only need to uncomment if running individual file
+# if __name__ == '__main__':
+#     # # Simulate across all 10 patients, giving each severities of 0.3, 0.6, and 0.9
+#     names = ["Cynthia", "Gus", "Joel", "Nathan", "Rick", "Hassan", "Soldier", "Jeff", "Carol", "Jane"]
+#     severities = [0.3, 0.6, 0.9]
+#     processes = []
+#
+# # Add a new thread for every patient at each severity, start each thread, and join them
+#     for name in names:
+#         for severity in severities:
+#             processes.append(mp.Process(None, COPD_ecmo, args=(name, severity)))
+#
+#     for p in processes:
+#         p.start()
+#
+#     for p in processes:
+#         p.join()
